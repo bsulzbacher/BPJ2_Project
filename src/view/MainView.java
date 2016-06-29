3689b520b2ac1d3c54d8b4c58522a1fc9d2f6343
bauer <bauer@DESKTOP-RROJF57>
2016-06-29 23:36:32 +0200

    local history vor pull

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -55,6 +55,23 @@ public class MainView{
 	private ObservableList<Schadensfall> sfList = FXCollections.observableArrayList();
 	private ObservableList<Material> matList = FXCollections.observableArrayList();
 	private ObservableList<KvItem> kvList = FXCollections.observableArrayList();
+	
+	//HARY Start
+//	
+//	private ObservableList<Kostenvoranschlag> kostenvoranschlagList = FXCollections.observableArrayList();
+//	
+//	public ObservableList<Kostenvoranschlag> getKostenvoranschlagList() {
+//		return kostenvoranschlagList;
+//	}
+//
+//	public void setKostenvoranschlagList(ObservableList<Kostenvoranschlag> kostenvoranschlagList) {
+//		this.kostenvoranschlagList = kostenvoranschlagList;
+//	}
+	
+	
+	
+	//HARY End
+
 	private TableView tab;
 	private TextField anzMat;
 	private Button matHinzu;
@@ -291,69 +308,6 @@ public class MainView{
 		return button5;
 	}
 	
-	//---------------------------------------------------------------------------------------
-	//HARY Start:
-//	
-//    private ObservableList<ObservableList> data;
-//    private TableView tableview;
-//
-//	
-//	public void alleKVs() {
-//		Connection c;
-//		//data = FXCollections.observableList();
-//	  try{
-//            c = DBConnection.getConnection();
-//            //SQL FÜR ALLE SCHADENSFÄLLE
-//			// ist ein Test, wirkliches SQL später
-//            String SQL = "select * from Schadensfall;";
-//            ResultSet rs = c.createStatement().executeQuery(SQL);
-//            //ResultSet
-//			
-//			//Spalten dynamisch anfügen
-//       
-//            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
-//                //We are using non property style for making dynamic table
-//                final int j = i;               
-//                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
-//                col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                   
-//                    public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                             
-//                        return new SimpleStringProperty(param.getValue().get(j).toString());                       
-//                    }                   
-//                });
-//            
-//                tableview.getColumns().addAll(col);
-//                System.out.println("Column ["+i+"] ");
-//            }
-//            while(rs.next()){
-//                //Iterate Row
-//                ObservableList<String> row = null;
-//                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
-//                    //Iterate Column
-//                    row.add(rs.getString(i));
-//                }
-//                System.out.println("Row [1] added "+row );
-//                data.add(row);
-// 
-//            }
-// 
-//            //FINALLY ADDED TO TableView
-//            tableview.setItems(data);
-//          }catch(Exception e){
-//              e.printStackTrace();
-//              System.out.println("Error on Building Data");            
-//          }
-//      }     
-//      public void start(Stage stage) throws Exception {
-//        //TableView
-//        tableview = new TableView();
-//        alleKVs();
-//        //Main Scene
-//        Scene scene = new Scene(tableview);       
-//        stage.setScene(scene);
-//        stage.show();
-//      }//HARY Ende
-////..............................................................................
-
 	public void zeichneSchadenfallerfassung() {
 		// Philipp
 		root.getChildren().clear();
